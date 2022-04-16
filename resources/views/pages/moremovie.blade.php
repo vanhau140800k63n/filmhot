dad3a2741b2fedcb3170f0605ab6e2bfec185ecd
vanhau <vanhau140800@gmail.com>
2022-04-16 17:50:34 +0700

    update seo

--- resources/views/pages/moremovie.blade.php
+++ resources/views/pages/moremovie.blade.php
@@ -1,6 +1,18 @@
 @extends('layouts.master')
 @section('meta')
-<title>{{$result['homeSectionName']}}</title>
+<meta name="description" content="{{$result['homeSectionName']}} với các phim có phụ đề vietsub và chất lượng hình ảnh fullhd, và các bộ phim mới được phát hành hàng ngày! - topfilm">
+<meta name="keywords" content="topfilm, topphim, top film, top phim, phim vietsub, fullhd, full hd, phim moi nhat, phim hot, hen ho chon cong so, phim hay, top, film, hot phim, hot film, chieu rap, phim tam ly, devsne">
+<meta name="robots" content="index, follow">
+<meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">
+<meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">
+<meta property="og:locale" content="vi_VN">
+<meta property="og:type" content="website">
+<meta property="og:title" content="{{$result['homeSectionName']}} - Xem phim FullHD Vietsub">
+<meta property="og:description" content="{{$result['homeSectionName']}} với các phim có phụ đề vietsub và chất lượng hình ảnh fullhd, và các bộ phim mới được phát hành hàng ngày! - topfilm">
+<meta property="og:url" content="https://topfilm.devsne.vn/">
+<meta property="og:site_name" content="topfilm">
+<meta property="og:image" content="">
+<title>{{$result['homeSectionName']}} - Xem phim FullHD Vietsub</title>
 @endsection
 @section('content')
 <div class="box advanced"> 
--- resources/views/pages/movie.blade.php
+++ resources/views/pages/movie.blade.php
@@ -23,20 +23,23 @@
 				<video class="movie__screen" id="video_media" autoplay crossorigin controls>
 					@foreach($movie_detail['episodeVo'][$episode_id]['subtitlingList'] as $subtitle)
 					@if($subtitle['languageAbbr'] == 'vi')
-					<track id="subtitles" kind="subtitles" label="{{$subtitle['language']}}" srclang="{{$subtitle['languageAbbr']}}" src="https://srt-to-vtt.vercel.app/?url={{$subtitle['subtitlingUrl']}}" >
+					<track id="subtitles" kind="subtitles" label="{{$subtitle['language']}}" srclang="{{$subtitle['languageAbbr']}}" src="https://srt-to-vtt.vercel.app/?url={{$subtitle['subtitlingUrl']}}">
 					@endif
 					@endforeach
 				</video>
 				<i class="fa-solid fa-play movie__play"></i>
 				<div class="movie__load">
-					<div class="lds-ripple"><div></div><div></div></div>
+					<div class="lds-ripple">
+						<div></div>
+						<div></div>
+					</div>
 				</div>
 			</div>
 			<div class="movie__name" id="{{$movie_detail['name']}}">{{$movie_detail['name']}}</div>
 			<div class="movie__episodes">
 				@if($movie_detail['episodeCount'] > 1)
 				@foreach($movie_detail['episodeVo'] as $key => $episode)
-				<button class="episode" id="{{$key}}" >{{$key + 1}} </button>
+				<button class="episode" id="{{$key}}">{{$key + 1}} </button>
 				@endforeach
 				@endif
 			</div>
@@ -52,18 +55,24 @@
 			<div class="movie__intro">{{$movie_detail['introduction']}}</div>
 		</div>
 		<div class="movie__similar">
-			<?php $image = Session('image')?Session::get('image'):[]; ?>
-			@foreach($movie_detail['likeList'] as $movie) 
+			<?php $image = Session('image') ? Session::get('image') : []; ?>
+			@foreach($movie_detail['likeList'] as $movie)
 			<a class="similar__container" href="{{ route('movie.detail', ['category' => $movie['category'], 'id' => $movie['id']]) }}">
-				<?php 
-				$urlImage = 'img/'.$movie['category'].$movie['id'].'.jpg';
-				if(!file_exists($urlImage)) {
+				<?php
+				$urlImage = 'img/' . $movie['category'] . $movie['id'] . '.jpg';
+				if (!file_exists($urlImage)) {
 					$urlImage = $movie['coverVerticalUrl'];
-					$image[$movie['category'].$movie['id']] = $movie['coverVerticalUrl'];
+					$image[$movie['category'] . $movie['id']] = $movie['coverVerticalUrl'];
 				}
 				?>
 				<img src="{{asset($urlImage)}}">
-				<div class="similar__name">{{$movie['name']}}</div>
+				<div class="similar__name">
+					@if (trans()->has('search_advanced.detail.'. $item))
+					{{ __('search_advanced.detail.'. $item)}}
+					@else
+					{{$item}}
+					@endif
+				</div>
 			</a>
 			@endforeach
 			<?php Session()->put('image', $image); ?>
@@ -71,54 +80,54 @@
 	</div>
 </section>
 <script>
-    var video = document.getElementById('video_media');
+	var video = document.getElementById('video_media');
 
-    document.onkeydown = function(event) {
-    	switch (event.keyCode) {
-    		case 37:
-    		event.preventDefault();
+	document.onkeydown = function(event) {
+		switch (event.keyCode) {
+			case 37:
+				event.preventDefault();
 
-    		vid_currentTime = video.currentTime;
-    		video.currentTime = vid_currentTime - 5;
-    		break;
+				vid_currentTime = video.currentTime;
+				video.currentTime = vid_currentTime - 5;
+				break;
 
-    		case 39:
-    		event.preventDefault();
+			case 39:
+				event.preventDefault();
 
-    		vid_currentTime = video.currentTime;
-    		video.currentTime = vid_currentTime + 5;
-    		break;
-    	}
-    };
+				vid_currentTime = video.currentTime;
+				video.currentTime = vid_currentTime + 5;
+				break;
+		}
+	};
 
 	function restart() {
-		if(video.readyState == 0) {
+		if (video.readyState == 0) {
 			let episode_id = Number($('#media').attr('id_episode'));
 			let definition = $('.movie__quality').children(":selected").attr("id");
 			reload(episode_id, definition);
 		} else {
-            if(video.textTracks.length == 1) {
-            	video.textTracks[0].mode = 'hidden';
-            }
-            if(video.textTracks.length == 1) {
-            	video.textTracks[0].mode = 'showing';
-            }
+			if (video.textTracks.length == 1) {
+				video.textTracks[0].mode = 'hidden';
+			}
+			if (video.textTracks.length == 1) {
+				video.textTracks[0].mode = 'showing';
+			}
 			clearInterval(restart_media);
 		}
 	}
 
-	
-	$('.movie__similar img').css('max-height', $('.movie__similar img').width()*1.4);
+
+	$('.movie__similar img').css('max-height', $('.movie__similar img').width() * 1.4);
 	$('.movie__media').height($('.movie__media').width() * 1080 / 1920);
 	$('.movie__load').height($('.movie__media').height() + 5);
 
 	$('.episode').each(function() {
-		if($(this).attr('id') == $('#media').attr('id_episode')) {
+		if ($(this).attr('id') == $('#media').attr('id_episode')) {
 			$(this).css('background-color', '#ed5829');
 		}
 	})
 
-	
+
 	$('.movie__play').click(function() {
 		$('.movie__play').css('display', 'none');
 
@@ -126,7 +135,7 @@
 	})
 
 	function load() {
-        $('.movie__load').css('display','flex');
+		$('.movie__load').css('display', 'flex');
 
 		let _token = $('input[name="_token"]').val();
 		$.ajax({
@@ -143,7 +152,7 @@
 				definition: null,
 				_token: _token
 			}
-		}).done(function (data) {
+		}).done(function(data) {
 			$('#media').val(data['mediaUrl']);
 			window.history.pushState({}, '', data[2]);
 
@@ -161,26 +170,26 @@
 			// }
 			// $('.movie__quality').html(movie__quality);
 
-            let subtitle = '';
-            for(let i = 0; i < data['1'].length; ++i) {
-            	if(data['1'][i]['languageAbbr'] == 'vi') {
-            		subtitle = '<track kind="subtitles" label="' + data['1'][i]['language'] +'" srclang="' + data['1'][i]['languageAbbr'] +'" src="https://srt-to-vtt.vercel.app/?url='+ data['1'][i]['subtitlingUrl'] +'" >';
-            	}
-            }
-            $('.movie__screen').html(subtitle);
-            $('.movie__name').html($('.movie__name').attr('id') + data[3]);
-            $('.movie__load').css('display','none');
+			let subtitle = '';
+			for (let i = 0; i < data['1'].length; ++i) {
+				if (data['1'][i]['languageAbbr'] == 'vi') {
+					subtitle = '<track kind="subtitles" label="' + data['1'][i]['language'] + '" srclang="' + data['1'][i]['languageAbbr'] + '" src="https://srt-to-vtt.vercel.app/?url=' + data['1'][i]['subtitlingUrl'] + '" >';
+				}
+			}
+			$('.movie__screen').html(subtitle);
+			$('.movie__name').html($('.movie__name').attr('id') + data[3]);
+			$('.movie__load').css('display', 'none');
 
-            restart_media = setInterval(restart, 2000);
+			restart_media = setInterval(restart, 2000);
 
 			return true;
-		}).fail(function (e) {
+		}).fail(function(e) {
 			return false;
 		});
 	}
 
 	function reload(episode_id, definition) {
-		
+
 		let _token = $('input[name="_token"]').val();
 		$.ajax({
 			url: "{{ route('movie.episode-ajax')}}",
@@ -196,9 +205,9 @@
 				definition: definition,
 				_token: _token
 			}
-		}).done(function (data) {
+		}).done(function(data) {
 			var video = document.getElementById("video_media");
-			if(video.readyState != 0) {
+			if (video.readyState != 0) {
 				return true;
 			}
 			$('#media').val(data['mediaUrl']);
@@ -212,22 +221,22 @@
 			})(data['mediaUrl']);
 
 			let subtitle = '';
-            for(let i = 0; i < data['1'].length; ++i) {
-            	if(data['1'][i]['languageAbbr'] == 'vi') {
-            		subtitle = '<track kind="subtitles" label="' + data['1'][i]['language'] +'" srclang="' + data['1'][i]['languageAbbr'] +'" src="https://srt-to-vtt.vercel.app/?url='+ data['1'][i]['subtitlingUrl'] +'" >';
-            	}
-            }
-            $('.movie__screen').html(subtitle);
+			for (let i = 0; i < data['1'].length; ++i) {
+				if (data['1'][i]['languageAbbr'] == 'vi') {
+					subtitle = '<track kind="subtitles" label="' + data['1'][i]['language'] + '" srclang="' + data['1'][i]['languageAbbr'] + '" src="https://srt-to-vtt.vercel.app/?url=' + data['1'][i]['subtitlingUrl'] + '" >';
+				}
+			}
+			$('.movie__screen').html(subtitle);
 
 			return true;
-		}).fail(function (e) {
+		}).fail(function(e) {
 
 			return false;
 		});
 	}
 
 	function loadByDefinition(episode_id, definition) {
-		
+
 		let _token = $('input[name="_token"]').val();
 		$.ajax({
 			url: "{{ route('movie.episode-ajax')}}",
@@ -243,7 +252,7 @@
 				definition: definition,
 				_token: _token
 			}
-		}).done(function (data) {
+		}).done(function(data) {
 			$('#media').val(data['mediaUrl']);
 
 			((source) => {
@@ -255,17 +264,17 @@
 			})(data['mediaUrl']);
 
 			let subtitle = '';
-            for(let i = 0; i < data['1'].length; ++i) {
-            	if(data['1'][i]['languageAbbr'] == 'vi') {
-            		subtitle = '<track kind="subtitles" label="' + data['1'][i]['language'] +'" srclang="' + data['1'][i]['languageAbbr'] +'" src="https://srt-to-vtt.vercel.app/?url='+ data['1'][i]['subtitlingUrl'] +'" >';
-            	}
-            }
-            $('.movie__screen').html(subtitle);
+			for (let i = 0; i < data['1'].length; ++i) {
+				if (data['1'][i]['languageAbbr'] == 'vi') {
+					subtitle = '<track kind="subtitles" label="' + data['1'][i]['language'] + '" srclang="' + data['1'][i]['languageAbbr'] + '" src="https://srt-to-vtt.vercel.app/?url=' + data['1'][i]['subtitlingUrl'] + '" >';
+				}
+			}
+			$('.movie__screen').html(subtitle);
 
 			restart_media = setInterval(restart, 2000);
 
 			return true;
-		}).fail(function (e) {
+		}).fail(function(e) {
 
 			return false;
 		});
@@ -278,7 +287,7 @@
 		})
 		$(this).css('background', '#ed5829');
 		$('#media').attr('id_episode', $(this).attr('id'));
-        $('.movie__play').css('display', 'none');
+		$('.movie__play').css('display', 'none');
 
 		load();
 	})
