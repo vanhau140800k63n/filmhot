e298b957e365a87ec6b844e85af7c09d98e73f0c
vanhau <vanhau140800@gmail.com>
2022-04-23 07:22:27 +0700

    update

--- resources/views/pages/movie.blade.php
+++ resources/views/pages/movie.blade.php
@@ -96,7 +96,7 @@
 		console.log(video);
 
 		function restart() {
-			if (video['cache_']['duration'] == 0 || !video['controls_'] || video['error_'] != null) {
+			if (video['cache_']['duration'] == 0 || !video['controls_'] || video['error_'] != null || isNaN(video['cache_']['duration'])) {
 				let episode_id = Number($('#media').attr('id_episode'));
 				let definition = $('.movie__quality').children(":selected").attr("id");
 				reload(episode_id, definition);
