edbc8e7097297308d02fd5541de2a14d01772ecf
vanhau <vanhau140800@gmail.com>
2022-05-11 09:12:39 +0700

    update

--- app/Services/MovieService.php
+++ app/Services/MovieService.php
@@ -39,6 +39,35 @@ class MovieService
         return $convert['data'];
     }
 
+    public function getDataTest($url)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'lang: vi',
+                'versioncode: 11',
+                'clienttype: ios_jike_default'            ),
+        ));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+        $convert = json_decode($response, true);
+        if($convert == null) {
+            return null;
+        }
+
+        return $convert['data'];
+    }
+
     public function searchWithKeyWord($keyWord)
     {
         $curl = curl_init();
